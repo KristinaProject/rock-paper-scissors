c43a3c686643d9c44e4cccf4cdfc0f59e85e5e38
KristinaProject <kristtule@gmail.com>
2022-08-30 20:05:58 +0300

    feat: add round function, to play one round game

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 let value = ['rock', 'paper', 'scissors'];
+let computerChoice = getComputerChoice(0, value.length);
 
 function getComputerChoice(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   let random = Math.floor(Math.random() * (max - min) + min);
   return value[random];
+}
+
+function round(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+
+  if(playerSelection == computerSelection){
+    return 'Draw!';
+  } else if(playerSelection == value[1] && computerSelection == value[3] || playerSelection == value[2] && computerSelection == value[1] || playerSelection == value[3] && computerSelection == value[2]) {
+    return 'You are winner!';
+  } else {
+    return `You are looser! ${computerSelection} beats ${playerSelection}!`;
+  }
 }
