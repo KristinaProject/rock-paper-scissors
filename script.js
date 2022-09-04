8330424a95ba28c96b447c61024c456f59726254
KristinaProject <kristtule@gmail.com>
2022-09-04 21:49:58 +0300

    test: testing five games

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 let value = ["rock", "paper", "scissors"];
 let playerResult = 0;
 let compResult = 0;
-let playerChoice = '';
+let playerChoice = "";
+const message = document.querySelector("[data-game]");
+message.innerHTML = "";
+const reset = document.querySelector("[data-reset]");
+
+reset.addEventListener("click", resetGame());
+
+function resetGame() {
+  message.innerHTML = null;
+  compResult = 0;
+  playerResult = 0;
+}
 
 function getComputerChoice(min, max) {
   min = Math.ceil(min);
@@ -10,23 +21,48 @@ function getComputerChoice(min, max) {
   return value[random];
 }
 
+function getPlayerChoice() {
+  moreRounds(5);
+}
+
 function playRound(playerSelection, computerSelection) {
-  const message = document.querySelector("[data-winner]");
+  const plresult = document.querySelector("[data-plresult]");
+  const pcresult = document.querySelector("[data-pcresult]");
 
   if (playerSelection == computerSelection) {
-    return (message.innerHTML = "Tie!");
+    return (
+      (message.innerHTML = "Tie!"),
+      (plresult.innerHTML = playerResult),
+      (pcresult.innerHTML = compResult)
+    );
   } else if (playerSelection === value[1] && computerSelection == value[3]) {
     playerResult++;
-    return (message.innerHTML = "You are winner!");
+    return (
+      (message.innerHTML = "You are winner!"),
+      (plresult.innerHTML = playerResult),
+      (pcresult.innerHTML = compResult)
+    );
   } else if (playerSelection == value[2] && computerSelection == value[1]) {
     playerResult++;
-    return (message.innerHTML = "You are winner!");
+    return (
+      (message.innerHTML = "You are winner!"),
+      (plresult.innerHTML = playerResult),
+      (pcresult.innerHTML = compResult)
+    );
   } else if (playerSelection == value[3] && computerSelection == value[2]) {
     playerResult++;
-    return (message.innerHTML = "You are winner!");
+    return (
+      (message.innerHTML = "You are winner!"),
+      (plresult.innerHTML = playerResult),
+      (pcresult.innerHTML = compResult)
+    );
   } else {
     compResult++;
-    return (message.innerHTML = `You are looser! Computer choice ${computerSelection} beats yours ${playerSelection}!`);
+    return (
+      (message.innerHTML = `You are looser! Computer choice ${computerSelection} beats yours ${playerSelection}!`),
+      (plresult.innerHTML = playerResult),
+      (pcresult.innerHTML = compResult)
+    );
   }
 }
 
@@ -34,23 +70,36 @@ const rock = document.querySelector("[data-rock]");
 const paper = document.querySelector("[data-paper]");
 const scissors = document.querySelector("[data-scissors]");
 
-rock.addEventListener('click', () => {
+rock.addEventListener("click", () => {
   playerChoice = value[0];
-  oneRound();
-})
+});
 
-paper.addEventListener('click', () => {
+paper.addEventListener("click", () => {
   playerChoice = value[1];
-  oneRound();
-})
+});
 
-scissors.addEventListener('click', () => {
+scissors.addEventListener("click", () => {
   playerChoice = value[2];
-  oneRound();
-})
+});
 
 function oneRound() {
   const comp = getComputerChoice(0, value.length);
   playRound(playerChoice, comp);
   console.log(playerResult, compResult);
+}
+
+function moreRounds(num) {
+  const winner = document.querySelector("[data-winner]");
+  winner.innerHTML = '';
+
+  for (let i = 0; i <= num; i++) {
+    oneRound();
+
+    if (playerResult > compResult) {
+      winner.innerHTML = "You beat computer!";
+    } else {
+      winner.innerHTML = "Computer beat you!";
+    }
+  }
+  resetGame();
 }
